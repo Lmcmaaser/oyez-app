b341f1fb2d0779ec2a2f3779447f91efbdade7d5
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2020-06-23 13:42:04 -0700

    added static data

---
+++ src/static-data.js
@@ -0,0 +1,47 @@
+export default {
+  "states": [
+    {
+      "stateid": "a87213d2-b585-11ea-b3de-0242ac130004",
+      "state_name": "Arizona",
+      "zipid": "76bc21e6-b591-11ea-b3de-0242ac130004"
+    },
+    {
+     "stateid": "41c929d4-b587-11ea-b3de-0242ac130004",
+     "state_name": "Texas",
+     "zipid": "2a4ba37e-b58a-11ea-b3de-0242ac130004"
+    }
+  ],
+  "zip_codes": [
+    {
+      "zipid": "2a4ba37e-b58a-11ea-b3de-0242ac130004",
+      "code": 78145
+    },
+    {
+      "zipid": "76bc21e6-b591-11ea-b3de-0242ac130004",
+      "code": 85308
+    }
+  ],
+  "reports": [
+    {
+      "reportid": "39ad3418-b58a-11ea-b3de-0242ac130004",
+      "stateid": "41c929d4-b587-11ea-b3de-0242ac130004",
+      "zipid": "2a4ba37e-b58a-11ea-b3de-0242ac130004",
+      "date": "",
+      "household": 5
+    },
+    {
+      "reportid": "a469b766-b591-11ea-b3de-0242ac130004",
+      "stateid": "a87213d2-b585-11ea-b3de-0242ac130004"
+      "zipid": "76bc21e6-b591-11ea-b3de-0242ac130004",
+      "date": "",
+      "household": 2
+    },
+    {
+      "reportid": "b74fd9d2-b591-11ea-b3de-0242ac130004",
+      "stateid": "41c929d4-b587-11ea-b3de-0242ac130004",
+      "zipid": "2a4ba37e-b58a-11ea-b3de-0242ac130004",
+      "date": "",
+      "household": 3
+    }
+  ]
+}
